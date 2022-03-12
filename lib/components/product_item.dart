bdcae50b968e45e2b96e826dbf1157a88bc5a9d5
Luis Filipe de Oliveira Carvalho <luis.oliveira@setis.com.br>
2022-03-11 22:14:10 -0300

    implementado marcação de produto como favorito

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import '../models/product.dart';
 import '../utils/app_route.dart';
-import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final Product product;
-
-  const ProductItem(this.product);
-
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(15),
       child: GridTile(
@@ -28,10 +25,17 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: Icon(Icons.favorite, color: Theme.of(context).accentColor),
+          leading: IconButton(
+            onPressed: () => product.toogleFavorite(),
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            color: Theme.of(context).accentColor,
+          ),
           title: Text(product.title, textAlign: TextAlign.center),
-          trailing:
-              Icon(Icons.shopping_cart, color: Theme.of(context).accentColor),
+          trailing: Icon(
+            Icons.shopping_cart,
+            color: Theme.of(context).accentColor,
+          ),
         ),
       ),
     );
--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,35 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/models/product_list.dart';
-
-import '../models/product.dart';
-import '../data/dummy_data.dart';
-import '../components/product_item.dart';
+import '../components/product_grid.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   ProductOverviewScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Minha Loja"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: GridView.builder(
-          itemCount: loadedProducts.length,
-          itemBuilder: (ctx, index) => ProductItem(loadedProducts[index]),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-        ),
+        child: ProductGrid(),
       ),
     );
   }
